31c8dbb79f1525a3e048a45351b79f90aad5678f
JIUUOO <nninjiuuoo@gmail.com>
2024-08-02 14:37:44 +0900

    feat: 리액트 빌드 연결

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(cors());
 
 app.get("*", (req, res) => {
-  res.json("Hello World!");
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
