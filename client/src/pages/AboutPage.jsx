22fc28999988d851526ff6b9bf12954e1d5abf8b
JIUUOO <nninjiuuoo@gmail.com>
2024-08-02 14:31:12 +0900

    feat: 메인, 소개, 운영위원 페이지 추가

---
+++ client/src/pages/AboutPage.jsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "../components/layouts";
+
+export default function About() {
+  return (
+    <Container>
+      <Title>About!</Title>
+    </Container>
+  );
+}
---
+++ client/src/pages/CommitteePage.jsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "../components/layouts";
+
+export default function CommitteePage() {
+  return (
+    <Container>
+      <Title>Committee!</Title>
+    </Container>
+  );
+}
