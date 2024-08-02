6caa95536db079e60205ba8450bbaff607bb34d3
JIUUOO <nninjiuuoo@gmail.com>
2024-08-02 21:52:16 +0900

    feat: MongoDB 연결 구성

--- server/index.js
+++ server/index.js
@@ -2,14 +2,26 @@ const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const URI = process.env.MONGODB_ATLAS_URI;
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(cors());
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB not connected");
+    console.log(err);
+  });
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
