4bd5685157ad00ede6e15d063b173d126bd51c62
JIUUOO <nninjiuuoo@gmail.com>
2024-11-10 14:26:56 +0900

    feat: 언어 변경시 모바일 메뉴 닫기

--- client/src/hooks/CloseNavigation.jsx
+++ client/src/hooks/CloseNavigation.jsx
@@ -1,8 +1,11 @@
 import { useContext, useLayoutEffect } from "react";
 import { NavigationContext } from "../contexts/NavigationContext";
 import { useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function CloseNavigation() {
+  const { i18n } = useTranslation();
+
   const { setExpandMainMenu, setExpandSubMenu, setToggleModal } =
     useContext(NavigationContext);
 
@@ -12,7 +15,13 @@ export default function CloseNavigation() {
     setExpandMainMenu(null);
     setExpandSubMenu(null);
     setToggleModal(false);
-  }, [setExpandMainMenu, setExpandSubMenu, setToggleModal, pathname]);
+  }, [
+    setExpandMainMenu,
+    setExpandSubMenu,
+    setToggleModal,
+    pathname,
+    i18n.language,
+  ]);
 
   return null;
 }
